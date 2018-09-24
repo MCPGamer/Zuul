ea0ec16d97b7c0cafe32896c0ad90948974f5fad
godu <duarte.goncalves@aeis.ch>
2018-09-24 13:13:58 +0200

    Added Prequisites aswell as .gitignore stuff

---
+++ zuul/src/main/java/ch/bbw/zuul/zuul/zuul/Parser.java
@@ -0,0 +1,70 @@
+package ch.bbw.zuul.zuul.zuul;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * This class is the main class of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ * 
+ * This parser reads user input and tries to interpret it as an "Adventure"
+ * command. Every time it is called it reads a line from the terminal and
+ * tries to interpret the line as a two word command. It returns the command
+ * as an object of class Command.
+ *
+ * The parser has a set of known command words. It checks user input against
+ * the known commands, and if the input is not one of the known commands, it
+ * returns a command object that is marked as an unknown command.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 1.0 (February 2002)
+ */
+
+public class Parser 
+{
+
+    private CommandWords commands;  // holds all valid command words
+
+    public Parser() 
+    {
+        commands = new CommandWords();
+    }
+
+    public Command getCommand() 
+    {
+        String inputLine = "";   // will hold the full input line
+        String word1;
+        String word2;
+
+        System.out.print("> ");     // print prompt
+
+        BufferedReader reader = 
+            new BufferedReader(new InputStreamReader(System.in));
+        try {
+            inputLine = reader.readLine();
+        }
+        catch(java.io.IOException exc) {
+            System.out.println ("There was an error during reading: "
+                                + exc.getMessage());
+        }
+        String[] words = inputLine.split(" ");
+        word1 = word2 = null;
+        if ( words.length > 0 ) {
+            word1 = words[0];
+            if ( words.length > 1 ) {
+                word2 = words[1];
+            }
+        }
+        
+        // note: we just ignore the rest of the input line.
+
+        // Now check whether this word is known. If so, create a command
+        // with it. If not, create a "null" command (for unknown command).
+
+        if(commands.isCommand(word1))
+            return new Command(word1, word2);
+        else
+            return new Command(null, word2);
+    }
+}
